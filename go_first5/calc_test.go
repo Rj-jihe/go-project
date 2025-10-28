c499abf3b4da04c2946d14f8582e3b8df473a32c
Rj-jihe <j5753298511@qq.com>
2025-10-28 23:19:25 +0800

    first

---
+++ go_first5/calc_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) { //测试成功可以不用写
+	cases := []struct {
+		Name           string
+		A, B, Expected int
+	}{
+		{"a1", 2, 3, 5},
+		{"a2", 2, -3, -1},
+		{"a3", 2, 0, 2},
+	}
+	for _, s := range cases {
+		t.Run(s.Name, func(t *testing.T) {
+			if Add(s.A, s.B) != s.Expected {
+				t.Errorf("测试失败")
+				return
+			}
+		})
+	}
+}
